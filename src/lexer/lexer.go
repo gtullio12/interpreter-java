8b58a75e20976fbb2de8c10a103160560c634dd8
gtullio12 <gtullio12@protonmail.com>
2024-07-29 08:57:27 -1000

    Setup test and basic lexer structure

--- src/lexer/lexer.go
+++ src/lexer/lexer.go
@@ -1,1 +1,30 @@
 package lexer
+
+type Lexer struct {
+	value        string
+	position     int
+	readPosition int
+	ch           byte
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{
+		value: input,
+	}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.value) {
+		l.ch = 0
+	} else {
+		l.ch = l.value[l.position]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+func (l *Lexer) NextToken() {
+
+}
--- src/lexer/lexer_test.go
+++ src/lexer/lexer_test.go
@@ -1,9 +1,86 @@
 package lexer
 
 import (
+	"java/interpreter/tokens"
 	"testing"
 )
 
-func TestLexer(t *testing.T) {
+const javaCode = `
+	class Main {
+		public static void main(String[] args) {
+			int x = 5;
+			int y = 10;
+			String test = "Hello world";
+			System.out.println(test);
+			System.out.println(x + y);
+		}
 
+		public static int sum(int a, int b) {
+			return a + b;
+		}
+	}
+	`
+
+func TestLexer(t *testing.T) {
+	lexer := New(javaCode)
+	expectedResult := []tokens.Token{
+		tokens.Token{Type: tokens.CLASS, Literal: "class"},
+		tokens.Token{Type: tokens.IDENT, Literal: "Main"},
+		tokens.Token{Type: tokens.LBRACE, Literal: "{"},
+		tokens.Token{Type: tokens.PUBLIC, Literal: "public"},
+		tokens.Token{Type: tokens.STATIC, Literal: "static"},
+		tokens.Token{Type: tokens.VOID, Literal: "void"},
+		tokens.Token{Type: tokens.IDENT, Literal: "main"},
+		tokens.Token{Type: tokens.STRING, Literal: "String"},
+		tokens.Token{Type: tokens.LSPAREN, Literal: "["},
+		tokens.Token{Type: tokens.RSPAREN, Literal: "]"},
+		tokens.Token{Type: tokens.RBRACE, Literal: "}"},
+		tokens.Token{Type: tokens.INT, Literal: "int"},
+		tokens.Token{Type: tokens.IDENT, Literal: "x"},
+		tokens.Token{Type: tokens.ASSIGN, Literal: "="},
+		tokens.Token{Type: tokens.INT, Literal: "5"},
+		tokens.Token{Type: tokens.SEMICOLON, Literal: ";"},
+		tokens.Token{Type: tokens.INT, Literal: "int"},
+		tokens.Token{Type: tokens.IDENT, Literal: "y"},
+		tokens.Token{Type: tokens.ASSIGN, Literal: "="},
+		tokens.Token{Type: tokens.INT, Literal: "10"},
+		tokens.Token{Type: tokens.SEMICOLON, Literal: ";"},
+		tokens.Token{Type: tokens.STRING, Literal: "String"},
+		tokens.Token{Type: tokens.IDENT, Literal: "test"},
+		tokens.Token{Type: tokens.ASSIGN, Literal: "="},
+		tokens.Token{Type: tokens.STRING, Literal: "Hello world"},
+		tokens.Token{Type: tokens.SEMICOLON, Literal: ";"},
+		tokens.Token{Type: tokens.PRINT, Literal: "System.out.println"},
+		tokens.Token{Type: tokens.LPAREN, Literal: "("},
+		tokens.Token{Type: tokens.IDENT, Literal: "test"},
+		tokens.Token{Type: tokens.RPAREN, Literal: ")"},
+		tokens.Token{Type: tokens.SEMICOLON, Literal: ";"},
+		tokens.Token{Type: tokens.PRINT, Literal: "System.out.println"},
+		tokens.Token{Type: tokens.LPAREN, Literal: "("},
+		tokens.Token{Type: tokens.IDENT, Literal: "x"},
+		tokens.Token{Type: tokens.PLUS, Literal: "+"},
+		tokens.Token{Type: tokens.IDENT, Literal: "y"},
+		tokens.Token{Type: tokens.RPAREN, Literal: ")"},
+		tokens.Token{Type: tokens.SEMICOLON, Literal: ";"},
+		tokens.Token{Type: tokens.RBRACE, Literal: "}"},
+		tokens.Token{Type: tokens.PUBLIC, Literal: "public"},
+		tokens.Token{Type: tokens.STATIC, Literal: "static"},
+		tokens.Token{Type: tokens.INT, Literal: "int"},
+		tokens.Token{Type: tokens.IDENT, Literal: "sum"},
+		tokens.Token{Type: tokens.LPAREN, Literal: "("},
+		tokens.Token{Type: tokens.INT, Literal: "int"},
+		tokens.Token{Type: tokens.IDENT, Literal: "a"},
+		tokens.Token{Type: tokens.COMMA, Literal: ","},
+		tokens.Token{Type: tokens.INT, Literal: "int"},
+		tokens.Token{Type: tokens.IDENT, Literal: "b"},
+		tokens.Token{Type: tokens.RPAREN, Literal: ")"},
+		tokens.Token{Type: tokens.LBRACE, Literal: "{"},
+		tokens.Token{Type: tokens.RETURN, Literal: "return"},
+		tokens.Token{Type: tokens.IDENT, Literal: "a"},
+		tokens.Token{Type: tokens.PLUS, Literal: "+"},
+		tokens.Token{Type: tokens.IDENT, Literal: "b"},
+		tokens.Token{Type: tokens.SEMICOLON, Literal: ";"},
+		tokens.Token{Type: tokens.LBRACE, Literal: "}"},
+		tokens.Token{Type: tokens.LBRACE, Literal: "}"},
+	}
 }
