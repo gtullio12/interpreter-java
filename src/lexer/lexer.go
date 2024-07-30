18cdf51b89b186028b0e2e979266860579d1c7c9
gtullio12 <gtullio12@protonmail.com>
2024-07-29 14:09:01 -1000

    Implement most of lexer for Java interpreter

--- src/lexer/lexer.go
+++ src/lexer/lexer.go
@@ -1,5 +1,10 @@
 package lexer
 
+import (
+	"fmt"
+	"java/tokens"
+)
+
 type Lexer struct {
 	value        string
 	position     int
@@ -25,6 +30,104 @@ func (l *Lexer) readChar() {
 	l.readPosition += 1
 }
 
-func (l *Lexer) NextToken() {
+func (l *Lexer) NextToken() (tok tokens.Token) {
+	l.skipWhitespace()
 
+	switch l.ch {
+	case '+':
+		tok = tokens.Token{Type: tokens.PLUS, Literal: "+"}
+	case '-':
+		tok = tokens.Token{Type: tokens.MINUS, Literal: "-"}
+	case '=':
+		if l.peekChar() == '=' { // Was an `==`
+			tok = tokens.Token{Type: tokens.EQ, Literal: "=="}
+			l.readChar()
+		} else {
+			tok = tokens.Token{Type: tokens.ASSIGN, Literal: "="}
+		}
+	case '!':
+		if l.peekChar() == '=' {
+			l.readChar()
+			tok = tokens.Token{Type: tokens.NOT_EQ, Literal: "!="}
+		} else {
+			tok = tokens.Token{Type: tokens.BANG, Literal: "!"}
+		}
+	case '*':
+		tok = tokens.Token{Type: tokens.ASTERISK, Literal: "*"}
+	case ',':
+		tok = tokens.Token{Type: tokens.COMMA, Literal: ","}
+	case ';':
+		tok = tokens.Token{Type: tokens.SEMICOLON, Literal: ";"}
+	case '[':
+		tok = tokens.Token{Type: tokens.LSPAREN, Literal: "["}
+	case ']':
+		tok = tokens.Token{Type: tokens.RSPAREN, Literal: "]"}
+	case '{':
+		tok = tokens.Token{Type: tokens.LBRACE, Literal: "{"}
+	case '}':
+		tok = tokens.Token{Type: tokens.RBRACE, Literal: "}"}
+	case '(':
+		tok = tokens.Token{Type: tokens.LPAREN, Literal: "("}
+	case ')':
+		tok = tokens.Token{Type: tokens.RPAREN, Literal: ")"}
+	default:
+		fmt.Printf("Checking for character for %v\n", string(l.ch))
+		if isLetter(l.ch) {
+			literal := l.readIdentifier()
+			fmt.Printf("Found token identifier: %v\n", literal)
+			tokenType := tokens.LookupIdentifier(literal)
+			tok = tokens.Token{Type: tokenType, Literal: literal}
+			return
+		} else if isNumber(l.ch) {
+			literal := l.readNumber()
+			tok = tokens.Token{Type: tokens.INT, Literal: literal}
+			return
+		} else {
+			tok = tokens.Token{Type: tokens.ILLEGAL, Literal: string(l.ch)}
+			return
+		}
+	}
+
+	l.readChar()
+	return
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+		fmt.Printf("In readIdentifier. Current character: %v\n", string(l.ch))
+	}
+	return l.value[position:l.position]
+
+}
+
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isNumber(l.ch) {
+		l.readChar()
+	}
+	return l.value[position:l.position]
+}
+
+func isLetter(c byte) bool {
+	return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') && c != ' '
+}
+
+func isNumber(c byte) bool {
+	return c >= '0' && c <= '9'
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+func (l *Lexer) peekChar() byte {
+	if l.position >= len(l.value) {
+		return 0
+	} else {
+		return l.value[l.readPosition]
+	}
 }
--- src/lexer/lexer_test.go
+++ src/lexer/lexer_test.go
@@ -1,7 +1,7 @@
 package lexer
 
 import (
-	"java/interpreter/tokens"
+	"java/tokens"
 	"testing"
 )
 
@@ -21,66 +21,127 @@ const javaCode = `
 	}
 	`
 
-func TestLexer(t *testing.T) {
-	lexer := New(javaCode)
+// func TestLexerOneLine(t *testing.T) {
+//const smallJavaCode = `int x = 5;`
+
+//	lexer := New(smallJavaCode)
+//	expectedResult := []tokens.Token{
+//		tokens.Token{Type: tokens.INTEGER_DT, Literal: "int"},
+//		tokens.Token{Type: tokens.IDENT, Literal: "x"},
+//		tokens.Token{Type: tokens.ASSIGN, Literal: "="},
+//		tokens.Token{Type: tokens.INT, Literal: "5"},
+//		tokens.Token{Type: tokens.SEMICOLON, Literal: ";"},
+//	}
+//
+//	for _, tok := range expectedResult {
+//		result := lexer.NextToken()
+//		if result.Type != tok.Type {
+//			t.Errorf("For token: %v. Invalid token type! Token type should've been %v, but was %v\n", tok.Literal, tok.Type, result.Type)
+//		}
+//		if tok.Literal != result.Literal {
+//			t.Errorf("Invalid token literal! Token literal should've been %v, but was %v\n", tok.Literal, result.Literal)
+//		}
+//	}
+//}
+
+func TestLexerString(t *testing.T) {
+	const javaStringCode = `String str = "Hello world";`
+	lexer := New(javaStringCode)
 	expectedResult := []tokens.Token{
-		tokens.Token{Type: tokens.CLASS, Literal: "class"},
-		tokens.Token{Type: tokens.IDENT, Literal: "Main"},
-		tokens.Token{Type: tokens.LBRACE, Literal: "{"},
-		tokens.Token{Type: tokens.PUBLIC, Literal: "public"},
-		tokens.Token{Type: tokens.STATIC, Literal: "static"},
-		tokens.Token{Type: tokens.VOID, Literal: "void"},
-		tokens.Token{Type: tokens.IDENT, Literal: "main"},
-		tokens.Token{Type: tokens.STRING, Literal: "String"},
-		tokens.Token{Type: tokens.LSPAREN, Literal: "["},
-		tokens.Token{Type: tokens.RSPAREN, Literal: "]"},
-		tokens.Token{Type: tokens.RBRACE, Literal: "}"},
-		tokens.Token{Type: tokens.INT, Literal: "int"},
-		tokens.Token{Type: tokens.IDENT, Literal: "x"},
-		tokens.Token{Type: tokens.ASSIGN, Literal: "="},
-		tokens.Token{Type: tokens.INT, Literal: "5"},
-		tokens.Token{Type: tokens.SEMICOLON, Literal: ";"},
-		tokens.Token{Type: tokens.INT, Literal: "int"},
-		tokens.Token{Type: tokens.IDENT, Literal: "y"},
-		tokens.Token{Type: tokens.ASSIGN, Literal: "="},
-		tokens.Token{Type: tokens.INT, Literal: "10"},
-		tokens.Token{Type: tokens.SEMICOLON, Literal: ";"},
-		tokens.Token{Type: tokens.STRING, Literal: "String"},
-		tokens.Token{Type: tokens.IDENT, Literal: "test"},
+		tokens.Token{Type: tokens.STRING_DT, Literal: "String"},
+		tokens.Token{Type: tokens.IDENT, Literal: "str"},
 		tokens.Token{Type: tokens.ASSIGN, Literal: "="},
 		tokens.Token{Type: tokens.STRING, Literal: "Hello world"},
 		tokens.Token{Type: tokens.SEMICOLON, Literal: ";"},
-		tokens.Token{Type: tokens.PRINT, Literal: "System.out.println"},
-		tokens.Token{Type: tokens.LPAREN, Literal: "("},
-		tokens.Token{Type: tokens.IDENT, Literal: "test"},
-		tokens.Token{Type: tokens.RPAREN, Literal: ")"},
-		tokens.Token{Type: tokens.SEMICOLON, Literal: ";"},
-		tokens.Token{Type: tokens.PRINT, Literal: "System.out.println"},
-		tokens.Token{Type: tokens.LPAREN, Literal: "("},
-		tokens.Token{Type: tokens.IDENT, Literal: "x"},
-		tokens.Token{Type: tokens.PLUS, Literal: "+"},
-		tokens.Token{Type: tokens.IDENT, Literal: "y"},
-		tokens.Token{Type: tokens.RPAREN, Literal: ")"},
-		tokens.Token{Type: tokens.SEMICOLON, Literal: ";"},
-		tokens.Token{Type: tokens.RBRACE, Literal: "}"},
-		tokens.Token{Type: tokens.PUBLIC, Literal: "public"},
-		tokens.Token{Type: tokens.STATIC, Literal: "static"},
-		tokens.Token{Type: tokens.INT, Literal: "int"},
-		tokens.Token{Type: tokens.IDENT, Literal: "sum"},
-		tokens.Token{Type: tokens.LPAREN, Literal: "("},
-		tokens.Token{Type: tokens.INT, Literal: "int"},
-		tokens.Token{Type: tokens.IDENT, Literal: "a"},
-		tokens.Token{Type: tokens.COMMA, Literal: ","},
-		tokens.Token{Type: tokens.INT, Literal: "int"},
-		tokens.Token{Type: tokens.IDENT, Literal: "b"},
-		tokens.Token{Type: tokens.RPAREN, Literal: ")"},
-		tokens.Token{Type: tokens.LBRACE, Literal: "{"},
-		tokens.Token{Type: tokens.RETURN, Literal: "return"},
-		tokens.Token{Type: tokens.IDENT, Literal: "a"},
-		tokens.Token{Type: tokens.PLUS, Literal: "+"},
-		tokens.Token{Type: tokens.IDENT, Literal: "b"},
-		tokens.Token{Type: tokens.SEMICOLON, Literal: ";"},
-		tokens.Token{Type: tokens.LBRACE, Literal: "}"},
-		tokens.Token{Type: tokens.LBRACE, Literal: "}"},
 	}
+
+	for _, tok := range expectedResult {
+		result := lexer.NextToken()
+		if result.Type != tok.Type {
+			t.Errorf("For token: %v. Invalid token type! Token type should've been %v, but was %v\n", tok.Literal, tok.Type, result.Type)
+		}
+		if tok.Literal != result.Literal {
+			t.Errorf("Invalid token literal! Token literal should've been %v, but was %v\n", tok.Literal, result.Literal)
+		}
+	}
+
 }
+
+//func TestLexer(t *testing.T) {
+//	fmt.Println("Defining lexer")
+//	lexer := New(javaCode)
+//	expectedResult := []tokens.Token{
+//		tokens.Token{Type: tokens.CLASS, Literal: "class"},
+//		tokens.Token{Type: tokens.IDENT, Literal: "Main"},
+//		tokens.Token{Type: tokens.LBRACE, Literal: "{"},
+//		tokens.Token{Type: tokens.PUBLIC, Literal: "public"},
+//		tokens.Token{Type: tokens.STATIC, Literal: "static"},
+//		tokens.Token{Type: tokens.VOID, Literal: "void"},
+//		tokens.Token{Type: tokens.IDENT, Literal: "main"},
+//		tokens.Token{Type: tokens.STRING_DT, Literal: "String"},
+//		tokens.Token{Type: tokens.LSPAREN, Literal: "["},
+//		tokens.Token{Type: tokens.RSPAREN, Literal: "]"},
+//		tokens.Token{Type: tokens.RBRACE, Literal: "}"},
+//		tokens.Token{Type: tokens.INT, Literal: "int"},
+//		tokens.Token{Type: tokens.IDENT, Literal: "x"},
+//		tokens.Token{Type: tokens.ASSIGN, Literal: "="},
+//		tokens.Token{Type: tokens.INT, Literal: "5"},
+//		tokens.Token{Type: tokens.SEMICOLON, Literal: ";"},
+//		tokens.Token{Type: tokens.INT, Literal: "int"},
+//		tokens.Token{Type: tokens.IDENT, Literal: "y"},
+//		tokens.Token{Type: tokens.ASSIGN, Literal: "="},
+//		tokens.Token{Type: tokens.INT, Literal: "10"},
+//		tokens.Token{Type: tokens.SEMICOLON, Literal: ";"},
+//		tokens.Token{Type: tokens.STRING_DT, Literal: "String"},
+//		tokens.Token{Type: tokens.IDENT, Literal: "test"},
+//		tokens.Token{Type: tokens.ASSIGN, Literal: "="},
+//		tokens.Token{Type: tokens.STRING_DT, Literal: "Hello world"},
+//		tokens.Token{Type: tokens.SEMICOLON, Literal: ";"},
+//		tokens.Token{Type: tokens.SYSTEM, Literal: "System"},
+//		tokens.Token{Type: tokens.OUT, Literal: "out"},
+//		tokens.Token{Type: tokens.PRINTLN, Literal: "println"},
+//		tokens.Token{Type: tokens.LPAREN, Literal: "("},
+//		tokens.Token{Type: tokens.IDENT, Literal: "test"},
+//		tokens.Token{Type: tokens.RPAREN, Literal: ")"},
+//		tokens.Token{Type: tokens.SEMICOLON, Literal: ";"},
+//		tokens.Token{Type: tokens.SYSTEM, Literal: "System"},
+//		tokens.Token{Type: tokens.OUT, Literal: "out"},
+//		tokens.Token{Type: tokens.PRINTLN, Literal: "println"},
+//		tokens.Token{Type: tokens.LPAREN, Literal: "("},
+//		tokens.Token{Type: tokens.IDENT, Literal: "x"},
+//		tokens.Token{Type: tokens.PLUS, Literal: "+"},
+//		tokens.Token{Type: tokens.IDENT, Literal: "y"},
+//		tokens.Token{Type: tokens.RPAREN, Literal: ")"},
+//		tokens.Token{Type: tokens.SEMICOLON, Literal: ";"},
+//		tokens.Token{Type: tokens.RBRACE, Literal: "}"},
+//		tokens.Token{Type: tokens.PUBLIC, Literal: "public"},
+//		tokens.Token{Type: tokens.STATIC, Literal: "static"},
+//		tokens.Token{Type: tokens.INT, Literal: "int"},
+//		tokens.Token{Type: tokens.IDENT, Literal: "sum"},
+//		tokens.Token{Type: tokens.LPAREN, Literal: "("},
+//		tokens.Token{Type: tokens.INT, Literal: "int"},
+//		tokens.Token{Type: tokens.IDENT, Literal: "a"},
+//		tokens.Token{Type: tokens.COMMA, Literal: ","},
+//		tokens.Token{Type: tokens.INT, Literal: "int"},
+//		tokens.Token{Type: tokens.IDENT, Literal: "b"},
+//		tokens.Token{Type: tokens.RPAREN, Literal: ")"},
+//		tokens.Token{Type: tokens.LBRACE, Literal: "{"},
+//		tokens.Token{Type: tokens.RETURN, Literal: "return"},
+//		tokens.Token{Type: tokens.IDENT, Literal: "a"},
+//		tokens.Token{Type: tokens.PLUS, Literal: "+"},
+//		tokens.Token{Type: tokens.IDENT, Literal: "b"},
+//		tokens.Token{Type: tokens.SEMICOLON, Literal: ";"},
+//		tokens.Token{Type: tokens.LBRACE, Literal: "}"},
+//		tokens.Token{Type: tokens.LBRACE, Literal: "}"},
+//	}
+//	for _, token := range expectedResult {
+//		nextToken := lexer.NextToken()
+//		fmt.Printf("Token from lexer: %v. token from expectedResult: %v\n", nextToken, token)
+//		if nextToken.Type != token.Type {
+//			t.Errorf("For token: %v. Invalid token type! Token type should've been %v, but was %v\n", token.Literal, token.Type, nextToken.Type)
+//		}
+//		if nextToken.Literal != nextToken.Literal {
+//			t.Errorf("Invalid token literal! Token literal should've been %v, but was %v\n", token.Literal, nextToken.Literal)
+//		}
+//	}
+//}
