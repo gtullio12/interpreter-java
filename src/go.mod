62435e8c8004ac96d663308dea6505cabf4195f3
gtullio12 <gtullio12@protonmail.com>
2024-12-05 10:15:39 -1000

    update tokens

--- src/go.mod
+++ src/go.mod
@@ -1,3 +1,3 @@
-module java/interpreter
+module java
 
 go 1.22.4
---
+++ src/tokens/token.go
@@ -0,0 +1,94 @@
+package tokens
+
+type TokenType string
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT  = "IDENT" // add, foobar, x, y, ...
+	INT    = "INT"
+	STRING = "STRING"
+
+	// Operators
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+	PERIOD   = "."
+
+	LT = "<"
+	GT = ">"
+
+	EQ     = "=="
+	NOT_EQ = "!="
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN  = "("
+	RPAREN  = ")"
+	LSPAREN = "["
+	RSPAREN = "]"
+	LBRACE  = "{"
+	RBRACE  = "}"
+
+	// Keywords
+	TRUE   = "TRUE"
+	FALSE  = "FALSE"
+	IF     = "IF"
+	ELSE   = "ELSE"
+	RETURN = "RETURN"
+	CLASS  = "CLASS"
+	STATIC = "STATIC"
+
+	// Access modifiers
+	PUBLIC  = "PUBLIC"
+	PRIVATE = "PRIVATE"
+
+	// return type
+	VOID = "VOID"
+
+	// Data types
+	INTEGER_DT   = "int"
+	STRING_DT    = "String"
+	CHARACTER_DT = "char"
+
+	// External System library
+	SYSTEM  = "System"
+	OUT     = "OUT"
+	PRINTLN = "PRINTLN"
+)
+
+var keywords = map[string]TokenType{
+	"class":   CLASS,
+	"true":    TRUE,
+	"false":   FALSE,
+	"else":    ELSE,
+	"public":  PUBLIC,
+	"private": PRIVATE,
+	"static":  STATIC,
+	"void":    VOID,
+	"System":  SYSTEM,
+	"out":     OUT,
+	"println": PRINTLN,
+	"int":     INTEGER_DT,
+	"String":  STRING_DT,
+	"return":  RETURN,
+}
+
+func LookupIdentifier(s string) TokenType {
+	if tok, ok := keywords[s]; ok {
+		return tok
+	}
+	return IDENT
+}
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
