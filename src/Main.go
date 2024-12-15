5c07e0fb9c7d1091940a7f47077bd6ec76296944
gtullio12 <gtullio12@protonmail.com>
2024-12-14 15:06:36 -1000

    Add ASCII art java logo

--- src/Main.go
+++ src/Main.go
@@ -8,10 +8,19 @@ import (
 )
 
 func main() {
+	logo := `
+     ____.                    
+    |    |____ ___  _______   
+    |    \__  \\  \/ /\__  \  
+/\__|    |/ __ \\   /  / __ \_
+\________(____  /\_/  (____  /
+              \/           \/ 
+`
 	user, err := user.Current()
 	if err != nil {
 		panic(err)
 	}
+	fmt.Printf(logo, '\n')
 	fmt.Printf("Hello %s! This is the Java programming language!\n",
 		user.Username)
 	fmt.Printf("Feel free to type in commands\n")
