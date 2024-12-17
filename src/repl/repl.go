8e1d3d1155342b3ab91a5c789c9bca5fe9c78cf2
gtullio12 <gtullio12@protonmail.com>
2024-12-16 15:46:38 -1000

    Add evaluator to REPL

--- src/repl/repl.go
+++ src/repl/repl.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"java/evaluator"
 	"java/lexer"
 	"java/parser"
 )
@@ -31,8 +32,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 func printParserErrors(out io.Writer, errors []string) {
